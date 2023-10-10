7937201b43648efae120f488cf14a3fdfa61d40e
HaruGrossman <dogtaboodog@yahoo.com>
2023-10-10 15:41:57 -0500

    handleClick function

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import { useState } from 'react'
 import './App.css'
-import {puppyList} from './data.js'
+import { puppyList } from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  // console.log("Data: ", puppies)
 
-  console.log("Data: ", puppies)
+  function handleClick() {
+    onClick = {
+      ()=> { console.log("puppy id: ", puppy.id) }
+  }
+  // <p onClick={()} key={puppy.id}>{puppy.name} </p>
+
+
 
   return (
     <>
       {
         puppies.map((puppy) => {
-          return <p>{puppy.name}</p>
+          return <p key={puppy.id}>{puppy.name}</p>
         })
+
       }
     </>
   )
 }
+
 
 export default App
