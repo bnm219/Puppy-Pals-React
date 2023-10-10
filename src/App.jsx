8338cd6a39838bc6ad3a2b776f04b9a2b89bfcd8
Brittany Morris <Brittany.Morris32@gmail.com>
2023-10-10 16:13:56 -0400

    Added the puppy data and used map to display all of the puppy names.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+
+  console.log("Data: ", puppies)
 
   return (
     <>
-<h1>Hello World</h1>
+      {
+        puppies.map((puppy) => {
+          return <p>{puppy.name}</p>
+        })
+      }
     </>
   )
 }
